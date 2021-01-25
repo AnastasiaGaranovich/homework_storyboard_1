3383c32c45dc418d64f2ed853469aaa1234f6259
NastyaGaranovich <nastyamadness@gmail.com>
2021-01-25 20:31:15 +0300

    Create project, add 2 views

---
+++ Homework_Storyboard_1/Homework_Storyboard_1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Homework_Storyboard_1
+//
+//  Created by Анастасия Гаранович on 25.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
